af5867813d6138019b96a7021ac773866e0a77a8
ImIshan7 <ishandanu999@gmail.com>
2024-01-24 15:41:29 +0530

    Contend Updated - Watch Collection

--- view/Home.tsx
+++ view/Home.tsx
@@ -2,6 +2,12 @@ import {Component} from "react";
 import home from '../images/home/watchvintage.png';
 import home0 from '../images/home/vintagewatch02.png';
 import logo from '../images/LOGO.png';
+import product01 from '../images/product/product01.jpg';
+import product02 from '../images/product/product02.jpg';
+import product03 from '../images/product/product03.jpg';
+import product04 from '../images/product/prodcut04.jpg';
+import product05 from '../images/product/prodcut05.jpg';
+import product06 from '../images/product/product06.jpg';
 export class Home extends Component {
     render() {
         return (
@@ -120,6 +126,181 @@ export class Home extends Component {
                                 </div>
                             </div>
                         </div>
+                    </section>
+
+                    <div className="text-center p-10 bg-gray-800">
+                        <h1 className="text-5xl font-extrabold font-mono text-white ">latest Watch Collection</h1>
+
+                    </div>
+
+
+                    <section id="Projects"
+                             className="w-full mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14  mb-5 bg-gray-800">
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl ">
+                            <a href="#">
+
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product01}/>
+
+
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">OMEGA</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">Omega Seamaster 120m Watch</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$1350</p>
+                                        <del>
+                                            <p className="text-sm font-semibold text-red-600 cursor-auto ml-2">$1990</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                            <a href="#">
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product02}/>
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">OMEGA</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">Omega Seamaster 43mm Dial Come With Box</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$1199</p>
+                                        <del>
+                                            <p className="text-sm  font-semibold text-red-600 cursor-auto ml-2">$1690</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                            <a href="#">
+
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product03}/>
+
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">OMEGA</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">OMEGA Seamaster Aqua Terra 150m</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$1390</p>
+                                        <del>
+                                            <p className="text-sm text-red-600 font-semibold cursor-auto ml-2">$1900</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                            <a href="#">
+
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product04}/>
+
+
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">ROLEX</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">Rolex Datejust 126234 18K/SS 36MM Oyster Palm Green Dial Sport Watch</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$2900</p>
+                                        <del>
+                                            <p className="text-sm text-red-600 font-semibold cursor-auto ml-2">$4000</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                            <a href="#">
+
+
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product05}/>
+
+
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">ROLEX</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">Rolex datejust 116300l Men Vintage Watch</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$1990</p>
+                                        <del>
+                                            <p className="text-sm text-red-600 font-semibold cursor-auto ml-2">$3000</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
+
+                        <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                            <a href="#">
+
+                                <img className="h-80 w-72 object-cover rounded-t-xl" src={product06}/>
+
+                                <div className="px-4 py-3 w-72">
+                                    <span className="text-gray-400 mr-3 uppercase text-xs">ROLEX</span>
+                                    <p className="text-lg font-bold text-black truncate block capitalize">Mens Rolex Date Stainless Steel Watch</p>
+                                    <div className="flex items-center">
+                                        <p className="text-xl font-bold text-black cursor-auto my-3">$2200</p>
+                                        <del>
+                                            <p className="text-sm text-red-600 font-semibold cursor-auto ml-2">$3900</p>
+                                        </del>
+                                        <div className="ml-auto"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                                  fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                                            <path
+                                                d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                                        </svg></div>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+
+
                     </section>
 
                 </main>
