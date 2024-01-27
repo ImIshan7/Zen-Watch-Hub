1d7908912dddf87c68aaf77ff0d60bc15a20001a
ImIshan7 <ishandanu999@gmail.com>
2024-01-27 22:56:10 +0530

    Contend Updated - cart

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "./view/Home";
+import {ShoppingCart} from "./view/ShoppingCart";
 
 function App() {
   return (
     <div>
 
       <BrowserRouter>
-        <Home/>
+        {/*<Home/>*/}
+          {/*<ShoppingCart/>*/}
+
+          <Routes>
+              <Route path="/*" Component={Home}></Route>
+              <Route path="/cart" Component={ShoppingCart}></Route>
+          </Routes>
       </BrowserRouter>
     </div>
   );
--- view/Home.tsx
+++ view/Home.tsx
@@ -9,6 +9,7 @@ import product04 from '../images/product/prodcut04.jpg';
 import product05 from '../images/product/prodcut05.jpg';
 import product06 from '../images/product/product06.jpg';
 import product07 from '../images/home/rolex01.jpg';
+
 export class Home extends Component {
     render() {
         return (
@@ -66,7 +67,7 @@ export class Home extends Component {
                                     <a href="#contact" className="py-2 px-6 flex font-semibold">
                                         Contact
                                     </a>
-                                    <button className="py-4 px-1 relative border-2 border-transparent text-white rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+                                    <button  className="py-4 px-1 relative border-2 border-transparent text-white rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
                                         <svg className="h-8 w-8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                             <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                                         </svg>
