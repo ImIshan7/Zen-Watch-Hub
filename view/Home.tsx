25f4a9c1d6c68c45769160a95fc4303a89d0bbb4
ImIshan7 <ishandanu999@gmail.com>
2024-01-27 11:42:55 +0530

    Contend Updated - Customer Reviews

--- view/Home.tsx
+++ view/Home.tsx
@@ -35,7 +35,7 @@ export class Home extends Component {
                                     </div>
                                     <div className="mt-2 w-full flex-shrink-0 lg:mt-0 lg:w-auto">
                                         <a className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-teal-600 shadow-sm hover:bg-teal-50"
-                                           href="#pricing">Buy now
+                                           href="#product">Buy now
                                         </a>
                                     </div>
                                 </div>
@@ -43,7 +43,7 @@ export class Home extends Component {
                         </div>
                     </div>
                 </div>
-                <main className="dark:bg-gray-800 bg-white h-screen pt-16">
+                <main id="home" className="dark:bg-gray-800 bg-white h-screen pt-16">
 
                     <header className="h-20 sm:h-30 flex items-center z-30 w-full">
 
@@ -53,13 +53,13 @@ export class Home extends Component {
                             </div>
                             <div className="flex items-center">
                                 <nav className="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden bg-gray-600 rounded-md">
-                                    <a href="#" className="py-2 px-6 flex font-semibold">
+                                    <a href="#home" className="py-2 px-6 flex font-semibold">
                                         Home
                                     </a>
-                                    <a href="#" className="py-2 px-6 flex font-semibold">
+                                    <a href="#about" className="py-2 px-6 flex font-semibold">
                                         About
                                     </a>
-                                    <a href="#" className="py-2 px-6 flex font-semibold">
+                                    <a href="#product" className="py-2 px-6 flex font-semibold">
                                         Products
                                     </a>
                                     <a href="#" className="py-2 px-6 flex font-semibold">
@@ -108,7 +108,7 @@ export class Home extends Component {
                         </div>
                     </div>
 
-                    <section className="bg-gray-800">
+                    <section id="about" className="bg-gray-800">
                         <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
                             <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
                                 <div className="max-w-lg">
@@ -128,14 +128,14 @@ export class Home extends Component {
                         </div>
                     </section>
 
-                    <div className="text-center p-10 bg-gray-800">
+                    <div id="product" className="text-center p-10 bg-gray-800">
                         <h1 className="text-5xl font-extrabold font-mono text-white ">latest Watch Collection</h1>
 
                     </div>
 
 
                     <section id="Projects"
-                             className="w-full mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14  mb-5 bg-gray-800">
+                             className="w-full mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 bg-gray-800">
 
 
                         <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl ">
@@ -300,7 +300,89 @@ export class Home extends Component {
                             </a>
                         </div>
 
+                    </section>
 
+                    <section className="bg-gray-800 px-4 py-12 md:py-24">
+                        <div className="max-w-screen-xl mx-auto">
+                            <h2 className="  text-5xl font-extrabold font-mono text-white text-center  leading-none uppercase max-w-2xl mx-auto mb-12">Customer Reviews</h2>
+                            <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4 relative">
+                                <div className="bg-gray-200 rounded-lg p-8 text-center md:w-1/3">
+                                    <p className="font-bold uppercase">John Doe</p>
+                                    <p className="text-xl font-sans text-gray-700">This podcast is amazing! The storytelling and production
+                                        quality are top-notch. Fast Delivery!</p>
+                                    <div className="flex items-center justify-center space-x-2 mt-4">
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-200 rounded-lg p-8 text-center md:w-1/3">
+                                    <p className="font-bold uppercase">Jane Smith</p>
+                                    <p className="text-xl font-sans text-gray-700">This podcast kept me on the edge of my seat.It is Worth To This Price.
+                                        Good Seller !</p>
+                                    <div className="flex items-center justify-center space-x-2 mt-4">
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="bg-gray-200 rounded-lg p-8 text-center md:w-1/3">
+                                    <p className="font-bold uppercase">Emily Johnson</p>
+                                    <p className="text-xl font-sans text-gray-700">I can't get enough of this podcast!
+                                        Best Communication.Highly recommend!</p>
+                                    <div className="flex items-center justify-center space-x-2 mt-4">
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3 .921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784 .57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81 .588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                        <svg className="text-yellow-500 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                             fill="currentColor" stroke="currentColor">
+                                            <path
+                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3 .921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784 .57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81 .588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </section>
 
                 </main>
